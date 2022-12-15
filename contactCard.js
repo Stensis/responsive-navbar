7f07ee4b403f9af8769e10f8dcafb562d126aded
Stensis <irenenjuguna98@gmail.com>
2022-12-16 00:34:53 +0300

    added contact card && navbar

---
+++ contactCard.js
@@ -0,0 +1,15 @@
+
+
+    <div class="container">
+        <form>
+            <h2> GET IN TOUCH </h2>
+            <input type="text" id="name" placeholder="Enter Your Name" required>
+            <input type="email" id="email" placeholder="Enter Your Email" required>
+            <input type="phone" id="phone" placeholder="Enter Your Phone Number" required>
+            <input type="text" id="coupen" placeholder="Enter Coupen Code" required>
+
+            <textarea id="message" rows="4" placeholder="Write Something"></textarea>
+            <button type="submit">Send</button>
+        </form>
+    </div>
+   
